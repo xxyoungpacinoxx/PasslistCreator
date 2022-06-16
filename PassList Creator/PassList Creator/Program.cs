3d01aa8121b5b3a5a308f72fc4f4e00a512e5b4e
xxyoungpacinoxx <matinzareinik@gmail.com>
2022-06-16 23:22:26 +0430

    Create C# Folder

---
+++ PassList Creator/PassList Creator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PassList_Creator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
