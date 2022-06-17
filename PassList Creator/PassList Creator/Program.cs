4cd2b81e068e21e0b5920e6963291cf5f06638db
xxyoungpacinoxx <matinzareinik@gmail.com>
2022-06-17 16:45:00 +0430

    Fix SwitchSytem With OOP Classes

--- PassList Creator/PassList Creator/Program.cs
+++ PassList Creator/PassList Creator/Program.cs
@@ -17,17 +17,40 @@ namespace PassList_Creator
             do
             {
                 Console.Write
-                ("R. ReadFile \tW. WriteLine \nSelect --> ");
+                ("Menue --> R. ReadFile \tW. WriteLine \tE. Exit \nSelect -->");
                 char Menu = Convert.ToChar(Console.ReadLine());
                 switch (Menu)
                 {
                     case 'R':
-
+                        Console.WriteLine("\n\nReadFile Service Is Running ...");
+                        Console.Write("Inser File Adress: ");
+                        StreamR SR = new StreamR(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine(SR.ReadFile());
+                        Console.WriteLine("\nReadFile Service Is Finish!\n");
 
                         break;
 
                     case 'W':
+                        Console.WriteLine("\n\nWriteFile Service Is Running ...");
+                        Console.Write("Inser File Adress: ");
+                        string FileAdress = Console.ReadLine();
+                        Console.Write("Inser FileName & Fromat (ex: pass.txt): ");
+                        string FileName = Console.ReadLine();
+                        StreamW SW = new StreamW(FileAdress,FileName);
+                        Console.WriteLine();
+                        Console.WriteLine("Write: ");
+                        SW.FileData = Console.ReadLine();
+                        SW.WriteFile();
+                        break;
 
+                    case 'E':
+                        Console.Write("Are You Sure (Y/N): ");
+                        char exitkey = Convert.ToChar(Console.ReadLine());
+                        if (exitkey == 'Y')
+                        {
+                            Environment.Exit(0);
+                        }
                         break;
 
                     default:
